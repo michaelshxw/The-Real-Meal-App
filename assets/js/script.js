c8a3f42f75d6146be6f367a2834e46f2f81c221c
michaelshxw <michaelshxw@gmail.com>
2021-04-15 22:31:47 +0800

    final push before end of nights work

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,13 +3,18 @@ var screenOne = document.querySelector(".screen1")
 var screenTwo = document.querySelector(".screen2")
 var screenTwo = document.querySelector(".screen3")
 var screenThree = document.querySelector(".screen4")
+var regNow = document.querySelector("#reg-now")
+
+regNow.addEventListener("click", signUpHandler)
 
 
 
 
 
-function signUpHandler() {
-
-
+function signUpHandler(n){
+    console.log('hi');
+    signUpContainer.innerHTML = "";
+    var x = document.querySelector(".screen1");
+    x[n].style.display = "none";
 }
 
