3dfd646ff19857a312148dc97744cb19c57a85d5
michaelshxw <michaelshxw@gmail.com>
2021-04-15 22:06:22 +0800

    update style.css, index.html with form inputs, basic styling

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var signUpContainer = document.querySelector(".signup")
+var screenOne = document.querySelector(".screen1")
+var screenTwo = document.querySelector(".screen2")
+var screenTwo = document.querySelector(".screen3")
+var screenThree = document.querySelector(".screen4")
+
+
+
+
+
+function signUpHandler() {
+
+
+}
+
