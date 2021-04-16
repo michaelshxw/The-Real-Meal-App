5e35dbceb03815644c3dfd5502467c36a44e38e4
michaelshxw <michaelshxw@gmail.com>
2021-04-16 16:13:17 +0800

    updating comments in script.js and style.css for clarification for other users.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,4 @@
+//declare all variables
 var formContainer = document.querySelector(".screen")
 var screenOne = document.querySelector(".one")
 var screenTwo = document.querySelector(".two")
@@ -12,6 +13,7 @@ var cancelBtnThree = document.querySelector('#cancelThree')
 var regNow = document.querySelector("#reg-now")
 var regLater = document.querySelector("#reg-later")
 
+//register now button, clears current display, displays page two
 regNow.addEventListener("click", regNowSubmit)
 function regNowSubmit(){
     console.log('register now button clicked');
@@ -19,11 +21,14 @@ function regNowSubmit(){
     screenTwo.style.display = "block";
 }
 
+//register later button, skips sign up process, displays first page outside sign up (currently blank)
 regLater.addEventListener("click", regLaterSubmit)
 function regLaterSubmit(){
     console.log('register later button clicked');
     formContainer.style.display = "none";
 }
+
+//first next button, displays next page
 nextBtnOne.addEventListener("click", nextBtnOneSubmit)
 function nextBtnOneSubmit(){
     console.log('one');
@@ -31,6 +36,7 @@ function nextBtnOneSubmit(){
     screenThree.style.display = "block";
 }
 
+//second next button, displays next page
 nextBtnTwo.addEventListener("click", nextBtnTwoSubmit)
 function nextBtnTwoSubmit(){
     console.log('two');
@@ -38,35 +44,38 @@ function nextBtnTwoSubmit(){
     screenFour.style.display = "block";
 }
 
+//third next button, displays first page outside of sign up
 nextBtnThree.addEventListener("click", nextBtnThreeSubmit)
 function nextBtnThreeSubmit(){
     console.log('three');
     screenFour.style.display = "none";
 }
 
+//first cancel button, goes to page one of sign up
 cancelBtnOne.addEventListener("click", cancelBtnSubmitOne)
-cancelBtnTwo.addEventListener("click", cancelBtnSubmitTwo)
-cancelBtnThree.addEventListener("click", cancelBtnSubmitThree)
-
 function cancelBtnSubmitOne(){
     console.log('cancel');
     screenTwo.style.display = "none"
     screenOne.style.display = "block"
-    localStorage.clear()
+    localStorage.clear();
 }
 
+//second cancel button, goes to page one of sign up
+cancelBtnTwo.addEventListener("click", cancelBtnSubmitTwo)
 function cancelBtnSubmitTwo(){
     console.log('cancel');
     screenThree.style.display = "none"
     screenOne.style.display = "block"
-    localStorage.clear()
+    localStorage.clear();
 }
 
+//third cancel button, goes to page one of sign up
+cancelBtnThree.addEventListener("click", cancelBtnSubmitThree)
 function cancelBtnSubmitThree(){
     console.log('cancel');
     screenFour.style.display = "none"
     screenOne.style.display = "block"
-    localStorage.clear()
+    localStorage.clear();
 }
 
 console.log(localStorage);
