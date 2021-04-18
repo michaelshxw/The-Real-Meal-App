149030a380c650a3534fb19db32b032afc3cb62a
michaelshxw <michaelshxw@gmail.com>
2021-04-18 10:59:45 +0800

    final push before merging

--- assets/js/script.js
+++ assets/js/script.js
@@ -16,7 +16,6 @@ var regLater = document.querySelector("#reg-later")
 //register now button, clears current display, displays page two
 regNow.addEventListener("click", regNowSubmit)
 function regNowSubmit(){
-    console.log('register now button clicked');
     screenOne.style.display = "none";
     screenTwo.style.display = "block";
 }
@@ -24,37 +23,51 @@ function regNowSubmit(){
 //register later button, skips sign up process, displays first page outside sign up (currently blank)
 regLater.addEventListener("click", regLaterSubmit)
 function regLaterSubmit(){
-    console.log('register later button clicked');
     formContainer.style.display = "none";
 }
 
 //first next button, displays next page
 nextBtnOne.addEventListener("click", nextBtnOneSubmit)
 function nextBtnOneSubmit(){
-    console.log('one');
     screenTwo.style.display = "none";
     screenThree.style.display = "block";
+    var nameValue = document.getElementById('nameValue').value
+    localStorage.setItem("nameValue", JSON.stringify(nameValue))
+    var ageValue = document.getElementById('ageValue').value
+    localStorage.setItem("ageValue", JSON.stringify(ageValue))
+    var heightValue = document.getElementById('heightValue').value
+    localStorage.setItem("heightValue", JSON.stringify(heightValue))
+    var weightValue = document.getElementById('weightValue').value
+    localStorage.setItem("weightValue", JSON.stringify(weightValue))
+    var genderValue = document.getElementById('genderValue').value
+    localStorage.setItem("genderValue", JSON.stringify(genderValue))
 }
 
 //second next button, displays next page
 nextBtnTwo.addEventListener("click", nextBtnTwoSubmit)
 function nextBtnTwoSubmit(){
-    console.log('two');
     screenThree.style.display = "none";
     screenFour.style.display = "block";
+    var activityValue = document.getElementById('activityValue').value
+    localStorage.setItem("activityValue", JSON.stringify(activityValue))
+    var goalValue = document.getElementById('goalValue').value
+    localStorage.setItem("goalValue", JSON.stringify(goalValue))
+    var dietaryValue = document.getElementById('dietaryValue').value
+    localStorage.setItem("dietaryValue", JSON.stringify(dietaryValue))
 }
 
 //third next button, displays first page outside of sign up
 nextBtnThree.addEventListener("click", nextBtnThreeSubmit)
 function nextBtnThreeSubmit(){
-    console.log('three');
     screenFour.style.display = "none";
+    var allergyValue = document.getElementById('allergyValue').value
+    localStorage.setItem("allergyValue", JSON.stringify(allergyValue))
+    alert('All Data Saved')
 }
 
 //first cancel button, goes to page one of sign up
 cancelBtnOne.addEventListener("click", cancelBtnSubmitOne)
 function cancelBtnSubmitOne(){
-    console.log('cancel');
     screenTwo.style.display = "none"
     screenOne.style.display = "block"
     localStorage.clear();
@@ -63,7 +76,6 @@ function cancelBtnSubmitOne(){
 //second cancel button, goes to page one of sign up
 cancelBtnTwo.addEventListener("click", cancelBtnSubmitTwo)
 function cancelBtnSubmitTwo(){
-    console.log('cancel');
     screenThree.style.display = "none"
     screenOne.style.display = "block"
     localStorage.clear();
@@ -72,10 +84,20 @@ function cancelBtnSubmitTwo(){
 //third cancel button, goes to page one of sign up
 cancelBtnThree.addEventListener("click", cancelBtnSubmitThree)
 function cancelBtnSubmitThree(){
-    console.log('cancel');
     screenFour.style.display = "none"
     screenOne.style.display = "block"
     localStorage.clear();
 }
 
-console.log(localStorage);
+// //form page 1 save data
+
+var userOptions = {
+    name: localStorage.getItem('nameValue'),
+    age: localStorage.getItem('ageValue'),
+    height: localStorage.getItem('heightValue'),
+    weight: localStorage.getItem('weightValue'),
+    gender: localStorage.getItem('genderValue'),
+    activity: localStorage.getItem('activityValue'),
+    goal: localStorage.getItem('goalValue'),
+    diet: localStorage.getItem('dietaryValue')
+}
